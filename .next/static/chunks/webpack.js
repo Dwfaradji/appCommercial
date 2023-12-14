88311c83296a7cffde312789a88b9b97715bd29a
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2023-12-14 19:24:37 +0100

    fix bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c1959521ad795aa4"; }
+/******/ 		__webpack_require__.h = function() { return "a9f41385427fa37c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
